a8215800410426957175784c0fcb8d562994348a
Lema <igor.lema@ifood.com.br>
2019-05-30 18:59:56 -0300

    download resources

--- gatling-s3.sh
+++ gatling-s3.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+aws s3 cp --recusrive s3://$BUCKET_NAME/resources/ /opt/gatling/user-files/resources/
 aws s3 cp --recursive s3://$BUCKET_NAME/simulations/ /opt/gatling/user-files/simulations/
 sh /opt/gatling/bin/gatling.sh -s $SIMULATION
 aws s3 sync /opt/gatling/results/ s3://$BUCKET_NAME/results/
